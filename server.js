d1f52f8cb97df07aeb5de5e5e6aeafb92d3c1dab
ThaigoDev <thaigodeveloper@gmail.com>
2025-08-21 09:59:44 -0300

    att

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.post('/generate-speech', async (req, res) => {
         console.log("Gerando áudio para o texto:", text.substring(0, 50) + "...");
         const mp3 = await openai.audio.speech.create({
             model: "tts-1",
-            voice: "echo", // Voz 'alloy' é uma boa opção
+            voice: "ash", // Voz 'alloy' é uma boa opção
             input: text,
         });
         
